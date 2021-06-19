42baec1f30e3e168f4e4bfb43ed508fd59901a81
Karla Ramirez <karlalizette13@gmail.com>
2021-06-19 10:53:47 -0500

    Base de datos con muchso cambios

--- bañosCharlyBD.sql
+++ bañosCharlyBD.sql
@@ -1,75 +1,83 @@
 CREATE TABLE Producto(
-    Codigo_p int,
-    Nombre_p varchar(200),
+    Id VARCHAR(14),
     Tipo VARCHAR(200),
+    Nombre VARCHAR(200),
     Precio DECIMAL(7,2),
+    Descripcion VARCHAR(500),
     Existencia INT,
-    Resurtible CHAR(2),
-    Estado char(1) DEFAULT "A",
-    PRIMARY KEY(Codigo_p)
+    Resurtible BOOLEAN,
+    PRIMARY KEY(Id)
 );
 
 CREATE TABLE Empleado(
-    Codigo_EM INT AUTO_INCREMENT,
-    Nombre VARCHAR(350),
-    Puesto VARCHAR(350),
+    Id INT AUTO_INCREMENT,
+    Nombre VARCHAR(100),
+    Apellido_p VARCHAR(100),
+    Apellido_m VARCHAR(100),
+    Nombre_usuario VARCHAR(100),
+    Email VARCHAR(350),
+    Contrasenia VARCHAR(21),
+    Puesto INT,
     Salario DECIMAL(7,2),
-    email_empleado VARCHAR(350),
-    contraseña VARCHAR(21),
-    Estado char(1) DEFAULT "A",
-    PRIMARY KEY(Codigo_EM)
+    Activo BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY(Id)
 );
 
 CREATE TABLE Proveedor(
-    RFC_Proveedor VARCHAR(13),
-    Nombre_Ed VARCHAR(350),
-    Tel_ed INT,
-    direccion_ed VARCHAR(350),
-    Estado char(1) DEFAULT "A",
-    PRIMARY KEY(RFC_Proveedor)
+    Id INT AUTO_INCREMENT,
+    Rfc VARCHAR(13),
+    Nombre VARCHAR(350),
+    Telefono VARCHAR(13),
+    direccion VARCHAR(350),
+    Activo BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY(Id)
 );
 
-
-CREATE TABLE Compras(
-    Folio_Compra INT,
+CREATE TABLE Compra(
+    Id INT,
+    Folio INT, --conjunto de compras
     Producto INT,
-    Cantidad INT,
     Precio DECIMAL(7,2),
-    Subtotal DECIMAL(7,2),
-    IVA DECIMAL(7,2),
-    proveedor int,
-    PRIMARY KEY(folio_Compra),
-    FOREIGN KEY(Producto)REFERENCES Producto(Codigo_p),
-    FOREIGN KEY(proveedor)REFERENCES Proveedor(RFC_Proveedor)
-);
-
-CREATE TABLE Ventas(
-    Folio_venta INT,
-    Producto INT,
-    Color VARCHAR(200),
     Cantidad INT,
-    Subtotal DECIMAL(7,2),
-    IVA DECIMAL(7,2),
-    Total DECIMAL (7,2),
+    PRIMARY KEY(Id),
+    FOREIGN KEY(Folio)REFERENCES Ticket_compra(Folio),
+    FOREIGN KEY(Producto)REFERENCES Producto(Id)
+);
+
+CREATE TABLE Ticket_compra(
+    Folio INT,
     Fecha DATE,
+    Id_empleado int,
+    Id_proveedor int,
+    FOREIGN KEY(Id_empleado)REFERENCES Empleado(Id),
+    FOREIGN KEY(Id_proveedor)REFERENCES Proveedor(RFC)
+);
+
+CREATE TABLE Venta(
+    Id INT,
+    Folio INT AUTO_INCREMENT,
+    Producto INT,
+    Color VARCHAR(150),
+    Cantidad INT,
+    FOREIGN KEY(Folio)REFERENCES Ticket_venta(Folio),
+    PRIMARY KEY(Id),
+    FOREIGN KEY(Producto)REFERENCES Producto(Id),
+);
+
+CREATE TABLE Ticket_venta(
+    Folio INT,
+    Fecha DATE,
+    Id_empleado int,
     RFC_Cliente VARCHAR(13),
-    PRIMARY KEY(Folio_venta),
-    FOREIGN KEY(Producto)REFERENCES Producto(Codigo_p),
+    FOREIGN KEY(Id_empleado)REFERENCES Empleado(Id),
 );
 
-CREATE TABLE Efectua(
-    Empleado int,
-    Compra int,
-    FOREIGN KEY(Empleado)REFERENCES Empleado(Codigo_EM),
-    FOREIGN KEY(Compra)REFERENCES Compras(Folio_Compra)
-);
 
-CREATE TABLE Realiza(
-    id_empleado INT,
-    id_venta INT,
-    FOREIGN KEY(id_empleado)REFERENCES Empleado(Codigo_EM),
-    FOREIGN KEY(id_venta)REFERENCES VEntas(Folio_venta)
-);
+
+
+
+
+
 
 
 
